f2f08e5a2b9e247dcfcb3112b4a577053d68b3c3
fatih <muhammedfatihcinar00@gmail.com>
2025-02-24 14:20:04 +0300

    redux toolkit ve router dom eklendi

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Provider } from "react-redux";
+import {BrowserRouter} from "react-router-dom";
+import {store} from "./store/store";
+
+
 
 
 import App from "./App.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App></App>
+    <Provider store={store}>
+        <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
