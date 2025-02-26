1e3f36bfdc380ae47adb37cd75ce9af5f3b94960
fatih <muhammedfatihcinar00@gmail.com>
2025-02-26 23:30:20 +0300

    Logo İmport şekli değişti

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import './index.css'
 
 import Footer from "./components/layout/Footer.tsx";
-
+import truckLogo from "../src/assets/Truck.jpg";
 
 import AppRoutes from './routes/routes.tsx';
 import Header from './components/layout/Header.tsx';
@@ -14,7 +14,7 @@ function App() {
       <>
         <div className="app-container">
           <main className="main-content">
-              <Header appName="TruckLink"  logoUrl="src/assets/Truck.jpg"/>
+              <Header appName="TruckLink"  logoUrl={truckLogo}/>
           <AppRoutes/>
           </main>
           <Footer/>
